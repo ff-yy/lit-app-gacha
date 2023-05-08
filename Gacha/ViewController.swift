0c3c830b3d01c8775b32d425e88f0c664b4ad224
ff-yy <yuya12121@gmail.com>
2023-05-08 17:46:38 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by MAC on 2023/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
